900cb1a7018c6c5e66793a962c0cb7ce58c95b26
chejaysdv <ChEjaysdv@gmail.com>
2023-08-10 10:55:43 +0800

    updated

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ require("express-async-errors");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const winston = require("winston");
+const config = require("config");
+
+// if (!config.get("jwtPrivateKey")) {
+//   console.log("FATAL ERROR:jwtPrivateKey is not set");
+//   process.exit(1);
+// }
 
 require("./start/logging")();
 require("./start/routes")(app);
